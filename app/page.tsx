c243071e2751bfc832a48faa72cc6e2f86acf1f2
Marcus Albernaz <marcusgabriel.ptu@gmail.com>
2025-04-02 15:58:53 -0300

    Atualização das telas de Login - Concluídas!

--- app/page.tsx
+++ app/page.tsx
@@ -1,82 +1,65 @@
-'use client'; // Diretiva para renderizar no lado do cliente
+'use client';
 
 import { useState } from 'react';
-import ForgotPassword from '@/components/layout/ForgotPassword'; // Importando o novo componente de recuperação de senha
-import { Button } from "@headlessui/react";
+import { useRouter } from 'next/navigation';
+import ForgotPassword from '@/components/layout/ForgotPassword';
+import { SuccessMessage, ErrorMessage } from '@/components/layout/mensages';
+import InactiveUserMessage from '@/components/layout/invalidUser';
 
 export default function LoginPage() {
-  const [showPassword, setShowPassword] = useState(false); // Estado para mostrar/ocultar a senha
-  const [showForgotPassword, setShowForgotPassword] = useState(false); // Estado para exibir a tela de recuperação de senha
+  const [showPassword, setShowPassword] = useState(false);
+  const [showForgotPassword, setShowForgotPassword] = useState(false);
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+  const [showErrorMessage, setShowErrorMessage] = useState(false);
+  const [showInactiveMessage, setShowInactiveMessage] = useState(false);
 
-  const togglePassword = () => {
-    setShowPassword(!showPassword); // Alterna o estado de mostrar/ocultar a senha
-  };
+  const togglePassword = () => setShowPassword(!showPassword);
+  const handleForgotPassword = () => setShowForgotPassword(true);
+  const handleCancelForgotPassword = () => setShowForgotPassword(false);
+  const handleAcknowledged = () => setShowForgotPassword(false);
+  const router = useRouter();
+  const handleLogin = (event: React.FormEvent) => {
+    event.preventDefault();
+    const isValidLogin = Math.random() > 0.5;
 
-  const handleForgotPassword = () => {
-    setShowForgotPassword(true); // Exibe a tela de recuperação de senha
-  };
-
-  const handleCancelForgotPassword = () => {
-    setShowForgotPassword(false); // Fecha a tela de recuperação de senha
-  };
-
-  const handleAcknowledged = () => {
-    setShowForgotPassword(false); // Fecha a tela de recuperação e volta para a tela de login
+    if (isValidLogin) {
+      setShowSuccessMessage(true);
+      setShowErrorMessage(false);
+    } else {
+      setShowErrorMessage(true);
+      setShowSuccessMessage(false);
+    }
   };
 
   return (
-    <div className="flex flex-col min-h-screen">
-      {/* Header */}
+    <div className="flex flex-col min-h-screen relative">
       <header className="bg-[#FB844A] py-12 relative">
         <div className="flex justify-center">
-          <img 
-            src="img/Logo_Festival1.png" 
-            alt="Logo da Empresa" 
-            className="absolute top-[45px] w-[250px] h-auto" 
-          />
+          <img src="img/Logo_Festival1.png" alt="Logo da Empresa" className="absolute top-[45px] w-[250px] h-auto" />
         </div>
       </header>
 
-      {/* Conteúdo Central */}
-      <div className="flex-grow flex items-center justify-center" style={{ backgroundColor: '#2A1E48' }}>
+      <div className="flex-grow flex items-center justify-center bg-[#2A1E48]">
         <div className="w-full max-w-md p-8 rounded-2xl">
-          {/* Se o estado showForgotPassword for verdadeiro, mostra o componente ForgotPassword */}
           {showForgotPassword ? (
             <ForgotPassword onCancel={handleCancelForgotPassword} onAcknowledged={handleAcknowledged} />
           ) : (
             <>
-              {/* Banner Principal */}
               <div className="flex justify-center mb-6">
-                <img 
-                  src="img/Banner_Principal.png" 
-                  alt="Banner do Evento" 
-                  className="w-full h-auto object-contain sm:w-3/4 md:w-full" 
-                />
+                <img src="img/Banner_Principal.png" alt="Banner do Evento" className="w-full h-auto object-contain sm:w-3/4 md:w-full" />
               </div>
 
-              {/* Formulário de Login */}
-              <form className="mt-6 space-y-4">
+              <form className="mt-6 space-y-4" onSubmit={handleLogin}>
                 <div>
                   <label className="block text-sm font-medium text-gray-50">Email</label>
-                  <input
-                    type="email"
-                    className="mt-1 w-full p-2 border rounded-lg focus:ring focus:ring-indigo-500 bg-white text-black placeholder-gray-700"
-                    placeholder="Digite seu email"
-                  />
+                  <input type="email" className="mt-1 w-full p-2 border rounded-lg focus:ring focus:ring-indigo-500 bg-white text-black placeholder-gray-700" placeholder="Digite seu email" required />
                 </div>
                 <div>
                   <label className="block text-sm font-medium text-gray-50">Senha</label>
                   <div className="relative">
-                    <input
-                      type={showPassword ? "text" : "password"} // Alterna entre 'text' e 'password'
-                      className="mt-1 w-full p-2 border rounded-lg focus:ring focus:ring-indigo-500 bg-white text-black placeholder-gray-700"
-                      placeholder="Digite sua senha"
-                    />
-                    <button
-                      type="button"
-                      className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-700"
-                      onClick={togglePassword} // Função de alternar
-                    >
+                    <input type={showPassword ? "text" : "password"} className="mt-1 w-full p-2 border rounded-lg focus:ring focus:ring-indigo-500 bg-white text-black placeholder-gray-700" placeholder="Digite sua senha" required />
+                    <button type="button" className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-700" onClick={togglePassword}>
+                      {showPassword ? "🙈" : "👁️"}
                     </button>
                   </div>
                 </div>
@@ -85,25 +68,39 @@ export default function LoginPage() {
                     Esqueci minha senha!
                   </button>
                 </div>
-                <Button className="w-full bg-[#FD0078] text-white p-2 rounded-lg hover:bg-pink-700">
+                <button type="submit" className="w-full bg-[#FD0078] text-white p-2 rounded-lg hover:bg-pink-700">
                   Entrar
-                </Button>
+                </button>
+
+                {/* Botão para testar InactiveUserMessage */}
+                <button type="button" className="w-full bg-red-500 text-white p-2 rounded-lg hover:bg-red-600 mt-2" onClick={() => setShowInactiveMessage(true)}>
+                  Teste tela de Usuário Inativo
+                </button>
+
+                 {/* Botão para navegar para NewPasswordPage */}
+            <button
+              type="button"
+              className="w-full bg-blue-500 text-white p-2 rounded-lg hover:bg-blue-600 mt-2"
+              onClick={() => router.push('/novaSenha')}
+              
+            >
+              Ir para Nova Senha
+            </button>
               </form>
             </>
           )}
         </div>
       </div>
 
-      {/* Footer */}
       <footer className="bg-[#FFEED3] py-4">
         <div className="flex justify-center">
-          <img 
-            src="/img/Patrocinadores.png" 
-            alt="Patrocinadores" 
-            className="w-full sm:w-3/4 md:w-1/2 lg:w-1/3 h-auto object-contain" 
-          />
+          <img src="/img/Patrocinadores.png" alt="Patrocinadores" className="w-full sm:w-3/4 md:w-1/2 lg:w-1/3 h-auto object-contain" />
         </div>
       </footer>
+
+      {showSuccessMessage && <SuccessMessage message="Login realizado com sucesso!" onClose={() => setShowSuccessMessage(false)} />}
+      {showErrorMessage && <ErrorMessage message="Erro ao realizar login. Tente novamente." onClose={() => setShowErrorMessage(false)} />}
+      {showInactiveMessage && <InactiveUserMessage onClose={() => setShowInactiveMessage(false)} />}
     </div>
   );
 }
--- components/layout/ForgotPassword.tsx
+++ components/layout/ForgotPassword.tsx
@@ -1,68 +1,82 @@
-'use client'; // Diretiva para renderizar no lado do cliente
+'use client'; 
 
 import { Button } from "@headlessui/react";
 import { useState } from 'react';
 
-// Componente de Recuperação de Senha
 export default function ForgotPassword({ onCancel, onAcknowledged }: { onCancel: () => void, onAcknowledged: () => void }) {
-  const [isRecoverySent, setIsRecoverySent] = useState(false); // Estado para controlar quando o e-mail de recuperação é enviado
+  const [isRecoverySent, setIsRecoverySent] = useState(false);
+  const [email, setEmail] = useState('');
+  const [error, setError] = useState('');
 
-  const handleRecover = () => {
-    // Lógica para enviar o e-mail de recuperação (aqui pode ser feita a chamada de API, por exemplo)
-    setIsRecoverySent(true); // Quando o e-mail for enviado, altere o estado para true
+  const isValidEmail = (email: string) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
   };
 
-  const handleAcknowledged = () => {
-    onAcknowledged(); // Chama a função para fechar o modal e voltar à tela de login
+  const handleRecover = () => {
+    if (!email.trim()) {
+      setError('Por favor, insira um e-mail.');
+      return;
+    }
+    if (!isValidEmail(email)) {
+      setError('Por favor, insira um e-mail válido.');
+      return;
+    }
+
+    setError('');
+    setIsRecoverySent(true);
   };
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
       <div className="w-full max-w-md p-8 rounded-2xl bg-white">
         {isRecoverySent ? (
-          // Exibe a tela de confirmação de envio de e-mail
           <div className="text-center">
             <img 
-              src="/img/success_image.png" // Imagem de sucesso, substitua com a sua imagem
+              src="/img/success_image.png"
               alt="E-mail Enviado"
-              className="mx-auto mb-4 w-24 h-24" // Ajuste o tamanho da imagem conforme necessário
+              className="mx-auto mb-4 w-24 h-24"
             />
             <h2 className="text-2xl font-bold text-gray-700 mb-2">E-mail de Recuperação Enviado!</h2>
             <p className="text-sm text-gray-600 mb-4">
-              O E-mail de recuperação foi enviado. Caso não o encontre na Caixa de Entrada, confira sua Caixa de Spam.
+              O e-mail de recuperação foi enviado. Caso não o encontre na Caixa de Entrada, confira sua Caixa de Spam.
             </p>
             <Button
               className="w-full bg-[#43B7A3] text-white p-2 rounded-lg hover:bg-[#308274]"
-              onClick={handleAcknowledged} // Função para o botão "Entendido"
+              onClick={onAcknowledged}
             >
               Entendido!
             </Button>
           </div>
         ) : (
-          // Exibe o formulário de recuperação de senha
           <>
             <h2 className="text-2xl font-bold text-left mb-4">Recuperar Senha</h2>
-            <p className="text-justifi mb-6">No campo abaixo, determine o seu e-mail que está cadastrado no sistema.</p>
-            <form className="space-y-4">
+            <p className="text-justify mb-6">No campo abaixo, informe o seu e-mail cadastrado no sistema.</p>
+            <form className="space-y-4" onSubmit={(e) => e.preventDefault()}>
               <div>
                 <label className="block text-sm font-medium text-gray-700">Email</label>
                 <input
                   type="email"
                   className="mt-1 w-full p-2 border rounded-lg focus:ring focus:ring-indigo-500 bg-white text-black placeholder-gray-700"
                   placeholder="Digite seu email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
                 />
+                {error && <p className="text-red-500 text-sm mt-1">{error}</p>}
               </div>
               <div className="flex space-x-4">
-                <Button
+                <Button 
+                  type="submit"
                   className="w-full bg-[#43B7A3] text-white p-2 rounded-lg hover:bg-[#308274]"
-                  onClick={handleRecover} // Função para o botão "Recuperar"
+                  onClick={handleRecover}
                 >
                   Recuperar
                 </Button>
                 <Button
                   type="button"
                   className="w-full bg-gray-300 text-black p-2 rounded-lg hover:bg-gray-400"
-                  onClick={onCancel} // Chamando a função onCancel ao clicar no botão
+                  onClick={onCancel}
                 >
                   Cancelar
                 </Button>
